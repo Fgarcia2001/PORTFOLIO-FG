4686117f91b90d2e9caec58a56c6751fdc9e9f00
francog <franco2001garcia@gmail.com>
2024-02-07 02:37:09 -0300

    desglosando error

--- mi-portolio/src/components/informacion.js
+++ mi-portolio/src/components/informacion.js
@@ -24,7 +24,7 @@ import sequelize from "../assets/tecnologias/sequelize.png";
 
 import git from "../assets/tecnologias/git.png";
 
-import gitHub from "../assets/tecnologias/github.png";
+/* import gitHub from "../assets/tecnologias/github.png"; */
 /*
 import scrum from "../assets/tecnologias/scrum.jpeg"; */
 
