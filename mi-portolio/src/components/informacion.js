6e42519b814241cf19481ddd362e1a5fa848f6fe
francog <franco2001garcia@gmail.com>
2024-02-07 00:58:02 -0300

    try

--- mi-portolio/src/components/informacion.js
+++ mi-portolio/src/components/informacion.js
@@ -1,5 +1,5 @@
-/* import formula1 from "../assets/formula1Project.jpeg";
-import globalshapers from "../assets/globalshapers.jpeg";
+import formula1 from "../assets/formula1Project.jpeg";
+/*import globalshapers from "../assets/globalshapers.jpeg";
 import portfolio from "../assets/proyectoPortfolio.jpeg";
 import react from "../assets/tecnologias/react1.png";
 import redux from "../assets/tecnologias/redux1.png";
@@ -22,7 +22,7 @@ const projects = [
     name: "Formula1",
     description:
       "Aplicación full stack web dedicada a corredores de Formula 1, permite a los usuarios explorar y descubrir información sobre diferentes corredores a través de una interfaz intuitiva. Incluso si no están los puedes crear!",
-    image: "",
+    image: formula1,
     linkRepositorio: "https://github.com/Fgarcia2001/PI-DRIVERS",
     linkDeploy: "https://drivers-f1.vercel.app/",
     linkYoutube: "https://www.youtube.com/watch?v=9fklEkoyIG4&t=84s",
