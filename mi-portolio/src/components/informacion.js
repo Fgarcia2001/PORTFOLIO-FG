dfad50af83d82e5695b5cb849c6dec26e50fefab
francog <franco2001garcia@gmail.com>
2024-02-07 00:40:07 -0300

    finish

--- mi-portolio/src/components/informacion.js
+++ mi-portolio/src/components/informacion.js
@@ -1,4 +1,4 @@
-import formula1 from "../assets/formula1Project.jpeg";
+/* import formula1 from "../assets/formula1Project.jpeg";
 import globalshapers from "../assets/globalshapers.jpeg";
 import portfolio from "../assets/proyectoPortfolio.jpeg";
 import react from "../assets/tecnologias/react1.png";
@@ -12,7 +12,7 @@ import postgresql from "../assets/tecnologias/postgresql.png";
 import sequelize from "../assets/tecnologias/sequelize.png";
 import git from "../assets/tecnologias/git.png";
 import github from "../assets/tecnologias/github.png";
-import scrum from "../assets/tecnologias/scrum.jpeg";
+import scrum from "../assets/tecnologias/scrum.jpeg"; */
 export const linkedin = "https://www.linkedin.com/in/francogarcia2001/";
 export const githubLink = "https://github.com/Fgarcia2001";
 export const email = "franco2001garcia@gmail.com";
@@ -22,7 +22,7 @@ const projects = [
     name: "Formula1",
     description:
       "Aplicación full stack web dedicada a corredores de Formula 1, permite a los usuarios explorar y descubrir información sobre diferentes corredores a través de una interfaz intuitiva. Incluso si no están los puedes crear!",
-    image: formula1,
+    image: "",
     linkRepositorio: "https://github.com/Fgarcia2001/PI-DRIVERS",
     linkDeploy: "https://drivers-f1.vercel.app/",
     linkYoutube: "https://www.youtube.com/watch?v=9fklEkoyIG4&t=84s",
@@ -31,7 +31,7 @@ const projects = [
     name: "Globalshapers",
     description:
       " SPA para la ONG GlobalShapers de Córdoba donde el cliente puede gestionar la informacion de sus miembros, publicaciones, donaciones y proyectos a traves de un dashboard de administrador.",
-    image: globalshapers,
+    image: "",
     linkRepositorio: "https://github.com/GlobalShapersCordoba/FRONT-GSC",
     linkDeploy: "https://react-deploy-85351.web.app",
     linkYoutube: "https://www.youtube.com/watch?v=N-W9pibeTWw",
@@ -40,7 +40,7 @@ const projects = [
     name: "Porfolio",
     description:
       "En mi portfolio se podra saber un poco de mi pasion por el desarrollo web, proyectos, experiencias y mucho mas!",
-    image: portfolio,
+    image: "",
     linkRepositorio: "https://github.com/Fgarcia2001/MI-PORTFOLIO",
     linkDeploy: "#",
     linkYoutube: "",
@@ -50,51 +50,51 @@ const projects = [
 const tecnologias = [
   {
     name: "Javascript",
-    image: javascript,
+    image: "",
   },
   {
     name: "React",
-    image: react,
+    image: "",
   },
   {
     name: "Redux",
-    image: redux,
+    image: "",
   },
   {
     name: "html",
-    image: html,
+    image: "",
   },
   {
     name: "css",
-    image: css,
+    image: "",
   },
   {
     name: "tailwind",
-    image: tailwind,
+    image: "",
   },
   {
     name: "nodeExpress",
-    image: nodeExpress,
+    image: "",
   },
   {
     name: "postgresql",
-    image: postgresql,
+    image: "",
   },
   {
     name: "sequelize",
-    image: sequelize,
+    image: "",
   },
   {
     name: "git",
-    image: git,
+    image: "",
   },
   {
     name: "github",
-    image: github,
+    image: "",
   },
   {
     name: "Scrum",
-    image: scrum,
+    image: "",
   },
 ];
 const links = [
